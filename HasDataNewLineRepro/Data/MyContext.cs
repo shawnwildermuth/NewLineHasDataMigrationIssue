92a136b728440f24d2dd53a27fb68b4629727923
Shawn Wildermuth <shawn@wildermuth.com>
2018-07-11 15:48:35 -0400

    Initial Repro Case

---
+++ HasDataNewLineRepro/Data/MyContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HasDataNewLineRepro.Data
+{
+  public class MyContext : DbContext
+  {
+    private readonly IHostingEnvironment _env;
+
+    public MyContext(DbContextOptions<MyContext> options, IHostingEnvironment env) : base(options)
+    {
+      _env = env;
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      var arts = new List<Product>();
+
+      arts.Add(new Product()
+      {
+        Id = 1,
+        Title = "Scarf Works",
+        Price = 19.99,
+        Description = @"
+<div>
+  <p>This is a scarf and all that goes with that.</p>
+</div>"
+      });
+
+      // Just Carriage Return Doesn't work (this came from a Linux Line Endings Json File)
+      arts.Add(new Product()
+      {
+        Id = 2,
+        Title = "Belt Doesn't",
+        Price = 19.99,
+        Description = "<div>\n<p>This is a scarf and all that goes with that.</p>\n</div>"
+      });
+
+      modelBuilder.Entity<Product>()
+        .HasData(arts.ToArray());
+    }
+  }
+}
---
+++ HasDataNewLineRepro/Migrations/20180711194150_Initial.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HasDataNewLineRepro.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Price", "Title" },
+                values: new object[] { 1, @"
+<div>
+  <p>This is a scarf and all that goes with that.</p>
+</div>", 19.99, "Scarf Works" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Price", "Title" },
+                values: new object[] { 2, "<div>
+<p>This is a scarf and all that goes with that.</p>
+</div>", 19.99, "Belt Doesn't" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
